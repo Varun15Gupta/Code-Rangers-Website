da55ddd00b6ded813aaf6649fb2a3a2619971f30
SAMIRAN_47 <98637819+hackerX-Sam@users.noreply.github.com>
2026-03-10 22:30:48 +0530

    Toggle isLive property for event objects

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -15,6 +15,7 @@ const Events = () => {
       difficulty: "All levels",
       link: "https://www.namespace.world/events/2N5BNA",
       tentative: true,
+      isLive: true,
       
     },
     {
@@ -29,7 +30,6 @@ const Events = () => {
       difficulty: "All Levels",
       link: "https://forms.gle/1JYbtSHaowKCnAit9",
       tentative: true,
-      isLive: true,
       
     },
     {
